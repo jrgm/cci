c2a2c1eeac5060bb7d654b2d7d87b26c3516396a
John Morrison <jrgmorrison@gmail.com>
2020-08-07 20:33:29 -0700

    f

---
+++ scripts/install-branch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+ls -lart --full-time
+git rev-parse --short HEAD
+git rev-parse --abbrev-ref HEAD
+git log --format='%s' -1 | head -1 | perl -pe 's/%/%%/'
+pwd
+echo @@@
+git checkout master
+git reset --hard origin/master
+ls -lart --full-time
+git rev-parse --short HEAD
+git rev-parse --abbrev-ref HEAD
+git log --format='%s' -1 | head -1 | perl -pe 's/%/%%/'
