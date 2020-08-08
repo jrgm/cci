8376912cabeab080bd69c0236c9bf3d25f7b1c7d
John Morrison <jrgmorrison@gmail.com>
2020-08-08 10:37:31 -0700

    f

--- scripts/install-branch.sh
+++ scripts/install-branch.sh
@@ -6,8 +6,8 @@ git rev-parse --abbrev-ref HEAD
 git log --format='%s' -1 | head -1 | perl -pe 's/%/%%/'
 pwd
 echo @@@
-git checkout master
-git reset --hard origin/master
+git checkout this-branch
+git reset --hard origin/this-branch
 git remote -v
 git pull
 npm i
