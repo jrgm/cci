4858722f683a51483a309cd68adf42f68589a429
John Morrison <jrgmorrison@gmail.com>
2020-08-08 16:20:13 -0700

    f

---
+++ scripts/maybe-run-tests.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+set -x
+
+CONTENT_SERVER_DOMAIN="${1:-accounts.stage.mozaws.net}"
+
+# This script is called from a circleci job that uses {restore,save}_cache
+# to cache the previously __version__ sha. Note: the $PREVIOUS_SHA_FILE name
+# must match the path specified in save_cache of the calling job.
+PREVIOUS_SHA=""
+PREVIOUS_SHA_FILE="./v1-previous-fxa-content-version-commit-sha.txt"
+if [ -f "${PREVIOUS_SHA_FILE}" ]; then
+  PREVIOUS_SHA=$(cat "${PREVIOUS_SHA_FILE}")
+fi
+
+COMMIT_SHA=$(curl -s https://${CONTENT_SERVER_DOMAIN}/__version__ | perl -ne 'next unless /"commit":\s+"([^"]*)"/; print $1, $/')
+
+if [ -z "${COMMIT_SHA}" ]; then
+  # if no parseable response, bail out
+  exit 0
+fi
+
+if [ "${COMMIT_SHA}" ne "${PREVIOUS_SHA}" ]; then
+  # run the test
+  exit 0
+fi
+
+# save the current sha to the cached file path
+/bin/echo -n $COMMIT_SHA > "${PREVIOUS_SHA_FILE}"
+
